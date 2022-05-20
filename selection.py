62e81792ac19312ed4a6dcf2547db233dd49c992
synclair1 <57903016+synclair1@users.noreply.github.com>
2022-05-20 08:37:21 +0500

    Update selection.py

--- selection.py
+++ selection.py
@@ -3,25 +3,36 @@ import random
 from population import Population
 random.seed()
 
+# class Tournament(object):
+#     def __init__(self):
+#         return
+        
+#     def compete(self, chromosomes):
+        # a = chromosomes[np.random.randint(0, len(chromosomes)-1)]
+        # b = chromosomes[np.random.randint(0, len(chromosomes)-1)]
+        # sel1,sel2 = a.fitness, b.fitness
+
+        # if(sel1 > sel2):
+#             fittest, weakest = a, b
+#         else:
+#             fittest, weakest = b, a 
+
+#         rndNum = np.random.uniform(0, 1)
+#         selection_rate = 0.80
+
+#         if(rndNum < selection_rate):
+#             return fittest
+#         else:
+#             return weakest
+
 class Tournament(object):
     def __init__(self):
         return
-        
-    def compete(self, chromosomes):
+    def compete(self, chromosomes): 
         a = chromosomes[np.random.randint(0, len(chromosomes)-1)]
         b = chromosomes[np.random.randint(0, len(chromosomes)-1)]
         sel1,sel2 = a.fitness, b.fitness
-
         if(sel1 > sel2):
-            fittest, weakest = a, b
+            return self.Population.sort()[:chromosomes]
         else:
-            fittest, weakest = b, a 
-
-        rndNum = np.random.uniform(0, 1)
-        selection_rate = 0.80
-
-        if(rndNum < selection_rate):
-            return fittest
-        else:
-            return weakest
-
+            return self.Population.sort()[-chromosomes:]
